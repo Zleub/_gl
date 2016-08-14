f44a4c781660f6b910938fcdd91e2cbe3534e3a0
Zleub <debray.arnaud@gmail.com>
2016-08-14 22:56:55 +0200

    fixed color array fixed imagedata alloc

--- gl/renderer.c
+++ gl/renderer.c
@@ -11,11 +11,32 @@ void			init_renderer(t_renderer *r)
 	assign_shader(r, "res/std_vertex.glsl", "res/std_fragment.glsl");
 }
 
+#define COLOR_LEN 16
+static t_vec3f color_table[COLOR_LEN] = {
+	{ 0.,   0.,   0. },
+	{ 1.,   1.,   1. },
+	{ 1.,   0.,   0. },
+	{ 0.,   1.,   0. },
+	{ 0.,   0.,   1. },
+	{ 1.,   1.,   0. },
+	{ 0.,   1.,   1. },
+	{ 1.,   0.,   1. },
+	{ 0.75, 0.75, 0.75 },
+	{ 0.5,  0.5,  0.5 },
+	{ 0.5,  0.,   0. },
+	{ 0.5,  0.5,  0. },
+	{ 0.,   0.5,  0. },
+	{ 0.5,  0.,   0.5 },
+	{ 0.,   0.5,  0.5 },
+	{ 0.,   0.,   0.5 }
+};
+
 void			assign_shader(t_renderer *r, char *v_path, char *f_path)
 {
 	static int j = 0;
 
 	printf("-NEW WINDOW-------------------------------\n");
+
 	r->vertex_shader = make_shader(GL_VERTEX_SHADER, v_path);
 	r->fragment_shader = make_shader(GL_FRAGMENT_SHADER, f_path);
 
@@ -33,32 +54,19 @@ void			assign_shader(t_renderer *r, char *v_path, char *f_path)
 	glBindBuffer(GL_ARRAY_BUFFER, r->VBOP);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(t_vec3f) * r->vertices_nbr, r->v_pos, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(r->vpos_location);
-	glVertexAttribPointer(r->vpos_location, 3, GL_FLOAT, GL_FALSE, 0, (void*) 0);
+	glVertexAttribPointer(r->vpos_location, sizeof(t_vec3f) / sizeof(float), GL_FLOAT, GL_FALSE, 0, (void*) 0);
 
 	glGenBuffers(1, &r->VBOC);
 	glBindBuffer(GL_ARRAY_BUFFER, r->VBOC);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(t_vec3f) * r->vertices_nbr, r->v_col, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(r->vcol_location);
-	glVertexAttribPointer(r->vcol_location, 3, GL_FLOAT, GL_FALSE, 0, (void*) 0);
+	glVertexAttribPointer(r->vcol_location, sizeof(t_vec3f) / sizeof(float), GL_FLOAT, GL_FALSE, 0, (void*) 0);
 
 	glGenBuffers(1, &r->VBOT);
 	glBindBuffer(GL_ARRAY_BUFFER, r->VBOT);
 	glEnableVertexAttribArray(r->vtex_location);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(t_vec3f) * r->vertices_nbr, r->v_tex, GL_STATIC_DRAW);
-	glVertexAttribPointer(r->vtex_location, 3, GL_FLOAT, GL_FALSE, 0, (void*) 0);
-
-
-	unsigned int size = r->window_size.x * r->window_size.y ;
-	t_vec3f img[size];
-	for (unsigned int i = 0; i < size; ++i)
-	{
-		if (j == 0)
-			img[i] = (t_vec3f){ .r = 1., .g = 0., .b = 0.};
-		if (j == 1)
-			img[i] = (t_vec3f){ .r = 0., .g = 1., .b = 0.};
-		if (j == 2)
-			img[i] = (t_vec3f){ .r = 0., .g = 0., .b = 1.};
-	}
+	glVertexAttribPointer(r->vtex_location, 2, GL_FLOAT, GL_FALSE, 0, (void*) 0);
 
 	glGenTextures(1, &r->texture);
 	glBindTexture(GL_TEXTURE_2D, r->texture);
@@ -67,7 +75,16 @@ void			assign_shader(t_renderer *r, char *v_path, char *f_path)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
+	unsigned int size = r->window_size.x * r->window_size.y ;
+
+	t_vec3f *img = malloc(sizeof(t_vec3f) * size);
+	for (unsigned int i = 0; i < size; ++i)
+	{
+		img[i] = color_table[j % COLOR_LEN];
+	}
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, r->window_size.x, r->window_size.y, 0, GL_RGB, GL_FLOAT, img);
+	free(img);
+
 	glUniform1i(t, 0);
 
 	printf("r->vertex_shader: %d\n", r->vertex_shader);
@@ -86,11 +103,18 @@ extern float cam_w;
 int				render(t_window *window)
 {
 	if (glfwWindowShouldClose(window->w))
+	{
+		glfwMakeContextCurrent(0);
+		glfwDestroyWindow(window->w);
+		// STAILQ_REMOVE(&g_mlx_context.w_head, np, entry, entries);
+		// free(window);
+		// window = 0;
 		return (0);
+	}
 	else
 	{
 		glfwMakeContextCurrent(window->w);
-		if (window->flush) {
+		if (window->flush > 0) {
 			glClear(GL_COLOR_BUFFER_BIT);
 			window->flush -= 1;
 		}

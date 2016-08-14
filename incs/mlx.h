33599e4c1269a58134567974f8e7633cb967ecac
Zleub <debray.arnaud@gmail.com>
2016-08-14 22:58:34 +0200

    window array to window bsd queue

--- incs/mlx.h
+++ incs/mlx.h
@@ -13,16 +13,38 @@
 //      .+ydddddddddhs/.
 //          .-::::-`
 
+#ifndef MLX_H
+# define MLX_H
+
+#include <sys/queue.h>
+
 #define mlx_init init
 #define mlx_new_window new_window
 
 #define WINDOW_MAX 8
 
+struct s_size {
+	int		width;
+	int		height;
+};
+
+typedef struct s_window_list t_window_list;
+struct s_window_list {
+	t_window					w;
+	STAILQ_ENTRY(s_window_list)	next;
+};
+
 struct s_mlx_context
 {
 	unsigned int		mode;
 
-	unsigned int		window_nbr;
-	t_window			*windows[WINDOW_MAX];
-	t_window			*active_window;
+	struct s_size		screen_size;
+	struct s_size		vidmode_size;
+
+	unsigned int							window_nbr;
+	STAILQ_HEAD(window_head, s_window_list)	w_head;
+	struct window_head						*headp;
+	t_window								*active_window;
 } ;
+
+#endif

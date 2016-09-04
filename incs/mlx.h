9852b5d05c21786ca4140aa88ac56d985c004bf1
Zleub <debray.arnaud@gmail.com>
2016-09-04 06:34:13 +0200

    image_list definition

--- incs/mlx.h
+++ incs/mlx.h
@@ -1,6 +1,7 @@
 #ifndef MLX_H
 # define MLX_H
 
+#include <_gl.h>
 #include <sys/queue.h>
 
 struct s_size {
@@ -14,6 +15,14 @@ struct s_window_list {
 	STAILQ_ENTRY(s_window_list)	next;
 };
 
+typedef struct s_image_list t_image;
+struct s_image_list {
+	struct s_size				size;
+	char						*data;
+	unsigned int				ref;
+	STAILQ_ENTRY(s_image_list)	next;
+};
+
 typedef struct s_mlx_context t_mlx_context;
 struct s_mlx_context
 {
@@ -24,20 +33,24 @@ struct s_mlx_context
 
 	unsigned int							window_nbr;
 	STAILQ_HEAD(window_head, s_window_list)	w_head;
+	STAILQ_HEAD(image_head, s_image_list)	i_head;
 	struct window_head						*headp;
 	t_window								*active_window;
 } ;
 
-#define mlx_init init
-#define mlx_new_window new_window
-#define mlx_clear_window clear_window
-#define mlx_pixel_put pixel_put
+#define mlx_init				init
+#define mlx_new_window			new_window
+#define mlx_clear_window		clear_window
+#define mlx_pixel_put			pixel_put
+#define mlx_get_data_addr		get_data_addr
+#define mlx_put_image_to_window	put_image_to_window
 
-#define mlx_loop loop
+#define mlx_loop			loop
+#define mlx_loop_hook		loop_hook
 
 #define mlx_new_image new_image
 
-#define X_SIZE 16 // r->window_size.x
-#define Y_SIZE 16 // r->window_size.y
+#define X_SIZE r->window_size.x
+#define Y_SIZE r->window_size.y
 
 #endif
